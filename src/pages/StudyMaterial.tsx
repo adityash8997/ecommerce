cf6c56880b47e762712ccde918785dc466879ed3
Sessobin <mohantykrishna57@gmail.com>
2025-09-21 23:30:22 +0530

    m

--- src/pages/StudyMaterial.tsx
+++ src/pages/StudyMaterial.tsx
@@ -206,6 +206,7 @@ const availableSubjects =
     if (material && material.downloadUrl) {
       // Method 1: Open in new tab with PDF viewer
       window.open(material.downloadUrl, '_blank', 'noopener,noreferrer');
+      
     }
 
   } catch (error) {
