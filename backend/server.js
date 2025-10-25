f8045fc5a061f76b65b161fe9d613de1106feb1e
SATVIKsynopsis <futureiitianisme@gmail.com>
2025-10-25 18:49:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2541,5 +2541,5 @@ app.get('/api/service-visibility', async (req, res) => {
   
 
 /* ---------------------- SERVER ---------------------- */
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
